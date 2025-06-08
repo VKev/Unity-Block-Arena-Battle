97f1e8ce4312b35a0ffd5939dba9e7b8e189244f
Mai Thanh Long <ojtpartner@sendo.vn>
2025-06-08 10:31:39 +0700

    it works on my machine

---
+++ Assets/Scripts/Player/WorldPhaseHealthUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayerStateMachine
+{
+    public class WorldPhaseHealthUI : MonoBehaviour
+    {
+        [SerializeField] private Image fillImage;
+        [SerializeField] private Transform followTarget;
+        [SerializeField] private Vector3 offset;
+
+
+        public void SetHealth(float percent)
+        {
+            if (fillImage != null)
+                fillImage.fillAmount = Mathf.Clamp01(percent);
+        }
+
+        private void Awake()
+        {
+            if (fillImage == null)
+            {
+                Transform fill = transform.Find("HealthBarContainer/HealthBarBackground/HealthBarFill");
+                fillImage = fill?.GetComponent<Image>();
+
+                if (fillImage == null)
+                    Debug.LogError("[WorldPhaseHealthUI] Could not find fillImage.");
+            }
+        }
+    }
+}
